be408490f6c6c2ce36a0ed2b145b3498eedcbf7b
iras-admin <iras-admin@DESKTOP-3BIBI32.robonet.hs-karlsruhe.de>
2023-12-28 15:49:48 +0100

    --- automatic rebase to remote; old local SHA: 7abc6cf34bee50c1a0b512be3fb6736d36ee22e0 ---

--- src/fri_masterthesis_mirko/FRI.java
+++ src/fri_masterthesis_mirko/FRI.java
@@ -1,41 +1,102 @@
 package fri_masterthesis_mirko;
 
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
 
-import javax.inject.Inject;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import com.kuka.connectivity.fastRobotInterface.ClientCommandMode;
+import com.kuka.connectivity.fastRobotInterface.FRIConfiguration;
+import com.kuka.connectivity.fastRobotInterface.FRIJointOverlay;
+import com.kuka.connectivity.fastRobotInterface.FRISession;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
-import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.controllerModel.Controller;
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.motionModel.PositionHold;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.PositionControlMode;
 
 /**
- * Implementation of a robot application.
- * <p>
- * The application provides a {@link RoboticsAPITask#initialize()} and a 
- * {@link RoboticsAPITask#run()} method, which will be called successively in 
- * the application lifecycle. The application will terminate automatically after 
- * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
- * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
- * exception is thrown during initialization or run. 
- * <p>
- * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
- * {@link RoboticsAPITask#dispose()} method.</b> 
- * 
- * @see UseRoboticsAPIContext
- * @see #initialize()
- * @see #run()
- * @see #dispose()
+ * Creates a FRI Session.
  */
-public class FRI extends RoboticsAPIApplication {
-	@Inject
-	private LBR lBR_iiwa_7_R800_1;
+public class FRI extends RoboticsAPIApplication
+{
+    private Controller _lbrController;
+    private LBR _lbr;
+    private String _clientName;
 
-	@Override
-	public void initialize() {
-		// initialize your application here
-	}
+    PositionControlMode ctrMode = new PositionControlMode();
+    PositionHold posHold = new PositionHold(ctrMode, -1, TimeUnit.MINUTES);
+    FRIJointOverlay jointOverlay;
+    @Override
+    public void initialize()
+    {
+        _lbrController = (Controller) getContext().getControllers().toArray()[0];
+        _lbr = (LBR) _lbrController.getDevices().toArray()[0];
+        // **********************************************************************
+        // *** change next line to the FRIClient's IP address                 ***
+        // **********************************************************************
+        _clientName = "172.31.0.21";
+    }
 
-	@Override
-	public void run() {
-		// your application execution starts here
-		lBR_iiwa_7_R800_1.move(ptpHome());
-	}
+    @Override
+    public void run()
+    {
+        // configure and start FRI session
+        FRIConfiguration friConfiguration = FRIConfiguration.createRemoteConfiguration(_lbr, _clientName);
+        friConfiguration.setSendPeriodMilliSec(5);
+
+        getLogger().info("Creating FRI connection to " + friConfiguration.getHostName());
+        getLogger().info("SendPeriod: " + friConfiguration.getSendPeriodMilliSec() + "ms |"
+                + " ReceiveMultiplier: " + friConfiguration.getReceiveMultiplier());
+
+        FRISession friSession = new FRISession(friConfiguration);
+        FRIJointOverlay jointOverlay = new FRIJointOverlay(friSession);
+
+        // wait until FRI session is ready to switch to command mode
+        try
+        {
+            friSession.await(10, TimeUnit.SECONDS);
+        }
+        catch (final TimeoutException e)
+        {
+            getLogger().error(e.getLocalizedMessage());
+            friSession.close();
+            return;
+        }
+
+        getLogger().info("FRI connection established.");
+
+        // move to start pose
+        _lbr.move(ptp(Math.toRadians(90), .0, .0, Math.toRadians(90), .0, Math.toRadians(-90), .0));
+
+       
+		PositionControlMode ctrMode = new PositionControlMode();
+		posHold = new PositionHold(ctrMode, -1, TimeUnit.MINUTES);
+		jointOverlay = new FRIJointOverlay(friSession, ClientCommandMode.POSITION);
+		
+
+        //_medflange.setLEDRed(false);
+        //_medflange.setLEDGreen(true);
+        //BooleanIOCondition _buttonPressed = new BooleanIOCondition(_medflange.getInput("UserButton"), true);
+        
+        //if(res == 0 || res == 1) 
+        _lbr.move(posHold.addMotionOverlay(jointOverlay));
+        
+
+        // done
+        friSession.close();
+    }
+
+    /**
+     * main.
+     * 
+     * @param args
+     *            args
+     */
+    public static void main(final String[] args)
+    {
+        final FRI app = new FRI();
+        app.runApplication();
+    }
+
 }
